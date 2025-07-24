810f29f8bdea23abe6e6075b5434ea57cce51024
Mike <foreign.guy.mike@gmail.com>
2025-07-23 21:30:34 -0500

    readme

---
+++ core/src/main/java/com/distraction/jetsetgo/entity/Collectable.java
@@ -0,0 +1,42 @@
+package com.distraction.jetsetgo.entity;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.distraction.jetsetgo.Context;
+import com.distraction.jetsetgo.Utils;
+
+public class Collectable extends Entity {
+
+    public enum Type {
+        WATERMELON("watermelon", 100),
+        ;
+
+        public String name;
+        public int points;
+        Type(String name, int points) {
+            this.name = name;
+            this.points = points;
+        }
+    }
+
+    private final Type type;
+    private final TextureRegion image;
+
+    public Collectable(Context context, Type type, float x, float y) {
+        this.type = type;
+        this.image = context.getImage(type.name);
+        this.x = x;
+        this.y = y;
+        w = image.getRegionWidth();
+        h = image.getRegionHeight();
+    }
+
+    public int getPoints() {
+        return type.points;
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        Utils.drawCentered(sb, image, x, y);
+    }
+}
--- core/src/main/java/com/distraction/jetsetgo/screens/PlayScreen.java
+++ core/src/main/java/com/distraction/jetsetgo/screens/PlayScreen.java
@@ -2,7 +2,10 @@ package com.distraction.jetsetgo.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.distraction.jetsetgo.Constants;
 import com.distraction.jetsetgo.Context;
+import com.distraction.jetsetgo.entity.Collectable;
 import com.distraction.jetsetgo.entity.Particle;
 import com.distraction.jetsetgo.entity.Player;
 
@@ -14,15 +17,26 @@ public class PlayScreen extends Screen {
     private final Player player;
 
     private final List<Particle> particles;
+    private final List<Collectable> collectables;
+
+    private int score;
+
+    private BitmapFont font = new BitmapFont();
 
     public PlayScreen(Context context) {
         super(context);
 
         particles = new ArrayList<>();
+        collectables = new ArrayList<>();
 
         player = new Player(context, particles);
         player.x = 100;
         player.y = 100;
+
+        // test collectables
+        collectables.add(new Collectable(context, Collectable.Type.WATERMELON, 300, 300));
+        collectables.add(new Collectable(context, Collectable.Type.WATERMELON, 340, 340));
+        collectables.add(new Collectable(context, Collectable.Type.WATERMELON, 380, 380));
     }
 
     @Override
@@ -41,6 +55,15 @@ public class PlayScreen extends Screen {
 
         player.update(dt);
 
+        for (int i = 0; i < collectables.size(); i++) {
+            Collectable c = collectables.get(i);
+            if (player.intersects(c)) {
+                score += c.getPoints();
+                c.remove = true;
+            }
+            if (c.remove) collectables.remove(i--);
+        }
+
         for (int i = 0; i < particles.size(); i++) {
             Particle p = particles.get(i);
             p.update(dt);
@@ -52,9 +75,12 @@ public class PlayScreen extends Screen {
     public void render() {
         sb.begin();
         sb.setProjectionMatrix(uiCam.combined);
+        font.draw(sb, "score: " + score, 10, Constants.HEIGHT - 10);
+        font.draw(sb, (int) player.x + ", " + (int) player.y, 10, Constants.HEIGHT - 25);
         sb.setProjectionMatrix(cam.combined);
         for (Particle p : particles) p.render(sb);
         player.render(sb);
+        for (Collectable c : collectables) c.render(sb);
         sb.end();
     }
 
